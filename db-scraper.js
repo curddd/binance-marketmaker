808cbc8e765bef7c89da25e2633cb70c1af169e0
derhatwlan <taubstumm@gmail.com>
2020-09-12 11:31:53 +0200

    Fixed Database require to correct file

--- db-scraper.js
+++ db-scraper.js
@@ -2,8 +2,8 @@ const Binance = require('node-binance-api');
 const EventEmitter = require('events')
 
 const API = require('./api')
-const Databse = require('./database');
-const Database = require('../1105/node/database');
+const Database = require('./database');
+
 
 
 const binance = new Binance().options();
